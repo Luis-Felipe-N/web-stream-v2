324f501ba9e0a381bc85d6b88bb5c61bfe5eba04
luis.nunes <luisj2felipe09@gmail.com>
2024-03-31 22:54:25 -0300

    :sparkles: Create episole-list component

---
+++ src/components/episode-list.tsx
@@ -0,0 +1,44 @@
+import { getEpisodesBySeason } from '@/server/actions/get-episode-by-season'
+import { EpisodeT } from '@/types'
+import { useQuery } from '@tanstack/react-query'
+import Image from 'next/image'
+
+interface EpisodeListProps {
+  seasonId: string
+}
+
+export default function EpisodeList({ seasonId }: EpisodeListProps) {
+  const { data } = useQuery<EpisodeT[]>({
+    queryKey: [`episode@${seasonId}`],
+    queryFn: () => getEpisodesBySeason(seasonId),
+  })
+
+  console.log(data)
+
+  return (
+    <div className="mt-14">
+      <p className="text-xl font-semibold">Todos Episódios</p>
+      <ul className="mt-4 grid lg:grid-cols-4 md:grid-cols-3 gap-8">
+        {data &&
+          data.map((episode) => (
+            <li className="w-full" key={episode.id}>
+              <Image
+                width={450}
+                height={350}
+                src={episode.cover}
+                alt=""
+              ></Image>
+              <small className="block my-2 mb-1 font-semibold text-slate-300">
+                {episode.season.anime.title}
+              </small>
+
+              <p>
+                T{episode.season.title.split('  ')[1]} E
+                {episode.title.split(' ')[1]} - {episode.title}
+              </p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+}
