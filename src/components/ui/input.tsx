100998bd3641e9f8668fe7d7ceae8d94e7b3d6f4
luis.nunes <luisj2felipe09@gmail.com>
2024-04-02 23:38:53 -0300

    :triangular_flag_on_post: New shadcn components

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,67 @@
-import * as React from 'react'
-
 import { cn } from '@/lib/utils'
+import { forwardRef } from 'react'
+import { FieldError } from 'react-hook-form'
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  prefix?: string
+  errors?: FieldError
+}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const hasError = !!props.errors
+
+    if (props.prefix) {
+      return (
+        <div>
+          <div
+            className={cn(
+              `transition text-base flex h-16 w-full rounded-md border border-slate-700 bg-slate-800 px-6 py-2  ring-offset-slate-800 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${
+                hasError ? 'border-2 border-red-600' : ''
+              } `,
+              className,
+            )}
+          >
+            <span className="whitespace-nowrap text-zinc-400">
+              {props.prefix}
+            </span>
+            <input
+              type={type}
+              className="w-full h-full bg-transparent outline-0"
+              ref={ref}
+              {...props}
+            />
+          </div>
+          {hasError && (
+            <p className="text-start mt-2 text-sm text-red-400">
+              {props.errors?.message}
+            </p>
+          )}
+        </div>
+      )
+    }
+
     return (
-      <input
-        type={type}
-        className={cn(
-          'text-base flex h-16 w-full rounded-md border border-slate-700 bg-slate-800 px-6 py-2  ring-offset-slate-800 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          className,
+      <div>
+        <input
+          type={type}
+          className={cn(
+            `transition text-base flex h-16 w-full rounded-md border border-slate-700 bg-slate-800 px-6 py-2  ring-offset-slate-800 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${
+              hasError ? 'border border-red-700/40' : ''
+            }`,
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+
+        {hasError && (
+          <p className="text-start mt-2 text-sm text-red-400">
+            {props.errors?.message}
+          </p>
         )}
-        ref={ref}
-        {...props}
-      />
+      </div>
     )
   },
 )
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex font-bold h-14 w-full items-center justify-between rounded-md bg-slate-800 px-6 py-2 text-sm ring-offset-zinc placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      'flex font-bold h-14 w-full items-center justify-between rounded-md bg-slate-900 border px-6 py-2 text-sm ring-offset-zinc placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
       className,
     )}
     {...props}
@@ -75,7 +75,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        'relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md bg-slate-800 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        'relative z-50 border max-h-96 min-w-[8rem] overflow-hidden rounded-md bg-slate-900 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         position === 'popper' &&
           'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
         className,
@@ -86,7 +86,7 @@ const SelectContent = React.forwardRef<
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
         className={cn(
-          'p-1',
+          '',
           position === 'popper' &&
             'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]',
         )}
@@ -118,12 +118,12 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex h-14 text-slate-100 bg-slate-800 hover:bg-slate-700 w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-slate-700 focus:text-slate-100 data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      'relative flex h-14 text-zinc-100 bg-slate-900 hover:bg-slate-800 w-full cursor-default select-none items-center py-1.5 pl-12 pr-2 text-sm outline-none focus:bg-slate-800 focus:text-zinc-100 data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
       className,
     )}
     {...props}
   >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+    <span className="absolute left-6 flex h-3.5 w-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
         <Check className="h-4 w-4" />
       </SelectPrimitive.ItemIndicator>
