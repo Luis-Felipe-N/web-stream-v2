0c564568f37ecaf5e8f2bd6287a90f7506b3c443
luisnunes <luisj2felipe09@gmail.com>
2024-04-11 15:19:24 -0300

    ✨ Get anime action server

---
+++ src/server/actions/animes/get-anime.ts
@@ -0,0 +1,9 @@
+import { api } from '@/lib/api'
+
+export async function getAnime(keyword = "") {
+    const response = await api.get(`/animes?keyword=${keyword}`)
+
+    const responseJson = await response.data
+
+    return responseJson.animes
+}
