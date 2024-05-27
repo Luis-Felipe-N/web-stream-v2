8a17eb62ba96523fafb7f83c1dfa10df0c08ae41
luisnunes <luisj2felipe09@gmail.com>
2024-05-27 15:46:37 -0300

    ✨ A

--- src/app/(feature)/movie/show/[id]/page.tsx
+++ src/app/(feature)/movie/show/[id]/page.tsx
@@ -15,12 +15,10 @@ async function getMovie(id: string): Promise<MovieD> {
 
 export default async function Movie({ params }: MovieProps) {
   const movie = await getMovie(params.id)
-  const stream = await getKalturaStream(movie.id_kaltura)
 
-  console.log(stream)
   return (
     <div className='relative z-[9999999]'>
-      <Player movie={movie} stream={stream} />
+      <Player movie={movie} />
     </div>
   )
 }
