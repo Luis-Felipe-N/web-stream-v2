1b552b2742b5ee812700da81fbbea20c324b84b5
luisnunes <luisj2felipe09@gmail.com>
2024-11-18 17:50:49 -0300

    💄 Remove Image next

--- src/components/rail/rail-genres.tsx
+++ src/components/rail/rail-genres.tsx
@@ -42,14 +42,14 @@ export default function RailGenres({ title, query, genre }: RailProps) {
             <SwiperSlide className="swiper-slide-perview" key={anime.id}>
               <div className="aspect-[4/3] lg:w-[15rem] lg:h-[21.92rem] md:w-[10rem] md:h-[14.62rem] w-[8rem] h-[11.69rem] border-2 border-transparent hover:border-slate-100 transition">
                 <Link href={`/anime/${anime.slug}`}>
-                  <Image
-                    quality={100}
+                  <img
+                    // quality={100}
                     width={308}
                     height={404}
                     className="h-full w-full object-cover"
                     src={anime.cover}
                     alt=""
-                  ></Image>
+                  />
                 </Link>
               </div>
             </SwiperSlide>
